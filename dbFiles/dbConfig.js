b916198358e2daa58c93ab622afd2874f78921c3
NawImNate <jnhatch3@gmail.com>
2022-04-28 17:30:30 -0400

    setup database config key values

---
+++ dbFiles/dbConfig.js
@@ -0,0 +1,15 @@
+config = {
+  user: "jnhat",
+  password: "password",
+  server: "DESKTOP-0LKDJMM",
+  database: "sql_tutorial",
+  options: {
+    trustServerCertificate: true,
+    trustedConnection: true,
+    enableArithAbort: true,
+    instantcename: "SQLEXPRESS",
+  },
+  port: 1433,
+};
+
+module.exports = config;
