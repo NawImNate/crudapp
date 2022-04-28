f9c45d9bace3ede87e11d285274d9dc5cfe9b17b
NawImNate <jnhatch3@gmail.com>
2022-04-28 15:36:41 -0400

    useState, async, await, callback function onclick

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [returnedData, setReturnedData] = useState("Hello there.");
   const getData = async (url) => {
     const newData = await fetch(url, {
       method: "GET",
@@ -10,13 +12,19 @@ function App() {
       },
     }).then((res) => res.json());
     console.log(newData);
+    setReturnedData(newData.result);
   };
 
   //on load - getData, call it as a function, save it, and check inspection tool>console to see results
+  // deleted the call for getData which was calling over and over for the data.
 
-  getData("/api");
-
-  return <div className="App">test2</div>;
+  //define an onclick event with a arrow function as an object, passing the path of quit as the parameter for getData Then display the changes on the screen.
+  return (
+    <div className="App">
+      <button onClick={() => getData("/quit")}>Click</button>
+      {returnedData}
+    </div>
+  );
 }
 
 export default App;
